7af810d5b8b756cfc9a0ada35e676254d9d3b33d
NSinopoli <NSinopoli@gmail.com>
2011-11-04 16:42:49 -0400

    Update bash script to let it run from anywhere.

--- app/resource/script/alter_view_permissions.sh
+++ app/resource/script/alter_view_permissions.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo find ../../view -type d -exec chgrp http {} \;
-sudo find ../../view -type d -exec chmod g+w {} \;
+if [ -L $0 ] ; then
+    ME=$(readlink $0)
+else
+    ME=$0
+fi
+DIR=$(dirname $ME)
+
+sudo find $DIR/../../view -type d -exec chgrp http {} \;
+sudo find $DIR/../../view -type d -exec chmod g+w {} \;
