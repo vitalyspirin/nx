d7ae46ffac55cb0715b0c4dfd4c073b52c10aa15
NSinopoli <NSinopoli@gmail.com>
2011-11-04 16:58:01 -0400

    Fix Password Test.

--- nx/test/lib/PasswordTest.php
+++ nx/test/lib/PasswordTest.php
@@ -7,16 +7,14 @@ use nx\lib\Password;
 class PasswordTest extends \PHPUnit_Framework_TestCase {
 
     public function test_Hash_ReturnsUniqueValues() {
-        $password = new Password();
-
         $correct = 'test1234';
-        $hash = $password->get_hash($correct);
-        $check = $password->check($correct, $hash);
+        $hash = Password::get_hash($correct);
+        $check = Password::check($correct, $hash);
         $this->assertTrue($check, 'Hashing a password and then checking it
             does not return true!');
 
         $wrong = 'wrong!';
-        $check = $password->check($wrong, $hash);
+        $check = Password::check($wrong, $hash);
         $this->assertFalse($check, 'Hashing a password and then checking it
             with an incorrect password does not return false!');
     }
