de6ecafbc871ce905645849aed032bbd62027c3a
Nick Sinopoli <NSinopoli@gmail.com>
2011-11-17 23:16:55 -0500

    Make headers for curl call optional.

--- nx/lib/Rest.php
+++ nx/lib/Rest.php
@@ -22,18 +22,23 @@ class Rest {
     *
     *  @param string $url              The url to which to send the request.
     *  @param string $query_string     Contains the parameters to be sent.
+    *  @param array $headers           HTTP header fields, in the format of:
+    *                                  array('Content-type: text/plain',
+    *                                  'Content-length: 100');
 
     *  @access public
     *  @return string
     */
-    public static function post($url, $query_string, $headers) {
+    public static function post($url, $query_string, $headers = null) {
         $handler = curl_init();
         curl_setopt($handler, CURLOPT_HEADER, 0);
         curl_setopt($handler, CURLOPT_RETURNTRANSFER,1);
         curl_setopt($handler, CURLOPT_URL, $url);
         curl_setopt($handler, CURLOPT_POST, 1);
-        curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($handler, CURLOPT_POSTFIELDS, $query_string);
+        if ( !is_null($headers) ) {
+            curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
+        }
 
         $response = curl_exec($handler);
         curl_close($handler);
