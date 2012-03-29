2dfa32b3d2c9a5efc4dc53fedd1bb4bf90f9de2e
Nick Sinopoli <NSinopoli@gmail.com>
2012-03-28 23:48:42 -0400

    Reduce repository to just the framework.

--- app/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$routes = array(
-    array('get', '/', 'app\controller\Dashboard::index')
-);
-
-nx\lib\Router::set_routes($routes);
-
-?>
--- app/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require dirname(__DIR__) . '/config/bootstrap.php';
-
-    $dispatcher = new \nx\core\Dispatcher();
-    echo $dispatcher->handle(new \nx\core\Request());
-?>
